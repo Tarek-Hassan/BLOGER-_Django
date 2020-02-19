e21fece2c308f209b843b2f649d7ec55a12eebf9
Nouran <nourans.96@yahoo.com>
2020-02-19 13:56:41 +0200

    Added template to display all posts in a category

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from blog import views
 urlpatterns=[
     path('home', views.home),
+    path('allPosts', views.PostList.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from blog.models import Post
 
 # Create your views here.
 def home(request):
     return render(request,'blogviews/home.html')
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'blogviews/allPosts.html'
