b48e14f3c468c1de68cd3d544c8a610fb7533ff4
Nouran <nourans.96@yahoo.com>
2020-02-20 00:21:51 +0200

    Modified Posts list view

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from blog import views
 urlpatterns=[
     path('home', views.home),
-    path('allPosts', views.PostList.as_view()),
+    # path('allPosts', views.PostList.as_view()),
+    path('allPosts', views.post_list)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.views import generic
 from blog.models import Post
+from Bloger.settings import MEDIA_ROOT
 
 # Create your views here.
 def home(request):
     return render(request,'blogviews/home.html')
 
-class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
+# class PostList(generic.ListView):
+#     queryset = Post.objects.filter(status=1).order_by('-created_on')
+#     template_name = 'blogviews/allPosts.html'
+
+def post_list(request):
     template_name = 'blogviews/allPosts.html'
+    posts = Post.objects.all()
+    media = MEDIA_ROOT #ADD MEDIA_ROOT in settings.py
+
+    context = {'post_list': posts, 'media':media}
+
+    return render(request, template_name, context)
