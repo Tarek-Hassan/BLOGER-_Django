43137ff27df6e76078f55d352451064cf9b58b24
Tarek-Hassan <tarek446240@gmail.com>
2020-02-21 15:31:06 +0200

    @tarek 21/2 navbar&&check is_blocked&&is_admin

--- loginRegister/forms.py
+++ loginRegister/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
 
 
@@ -10,3 +10,12 @@ class RegisterForm(UserCreationForm):
         model=User
         fields=('username','first_name','last_name','email','password1','password2', )
 
+class AuthFormCheckStatus(AuthenticationForm):
+    print("***inclass")
+    
+    def confirm_login_allowed(self,user):
+        print("****user.is_actqwive++",user.is_staff)
+        print("****user.is_actqwive++",user.is_active)
+        if not user.is_active and not user.is_staff:
+            print("*****active check")
+            raise forms.ValidationError("YourAckjhesdjvmdfvmfdncount",code='inactive',)
--- loginRegister/urls.py
+++ loginRegister/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path,include
 from django.contrib.auth import views as authView
 from loginRegister import views
-# from django.conf import settings 
-# from django.conf.urls.static import static 
+from loginRegister import forms
+
 
 urlpatterns = [
-    path('login/', authView.LoginView.as_view(), name='login'),
+    path('login/', authView.LoginView.as_view(authentication_form=forms.AuthFormCheckStatus), name='login'),
     path('logout/', authView.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
     path('', views.index, name='home'),
