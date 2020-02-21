08deeebd7ad1e1fc33a70cad8b1eaaee0c4c6873
Nouran <nourans.96@yahoo.com>
2020-02-21 16:09:43 +0200

    Converted to sqlite db

--- Bloger/settings.py
+++ Bloger/settings.py
@@ -78,10 +78,8 @@ WSGI_APPLICATION = 'Bloger.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DB_blog',
-        'USER': 'root',
-        'PASSWORD':'admin',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
