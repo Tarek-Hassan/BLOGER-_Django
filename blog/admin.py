109a37ee1cd439a1d6e9f0e161038b01f5bd38dd
Ahmed-Abd-elhamid <eng.a.abdelhamid@outlook.com>
2020-02-23 22:16:03 +0200

    category form

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,7 @@ from blog.models import Post, Comment, Reply, Category, Subscribe
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'author', 'status', 'created_on', 'image', 'category')
+    list_display = ('title', 'slug', 'author', 'status', 'created_on', 'image')
     list_filter = ("status",)
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
@@ -19,8 +19,9 @@ class ReplyAdmin(admin.ModelAdmin):
     search_fields = ('name', 'body')
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'creator')
-    search_fields = ['name']
+    list_display = ('category_name', 'category_creator', 'created_on',)
+    list_filter = ('created_on',)
+    search_fields = ['category_name']
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comment, CommentAdmin)
