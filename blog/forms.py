a7db3ba71e62f4741198803354de3fef1e803a8f
Ahmed-Abd-elhamid <eng.a.abdelhamid@outlook.com>
2020-02-23 22:21:04 +0200

    some updates on code

--- blog/forms.py
+++ blog/forms.py
@@ -1,25 +1,8 @@
-from .models import Comment, Reply, Post, Category
+from .models import Comment
 from django import forms
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('body',)
-        labels = {'body': ''}
-
-class ReplyForm(forms.ModelForm):
-    class Meta:
-        model = Reply
-        fields = ('body',)
-        labels = {'body': ''}
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model=Post
-        fields=('title','content','category','image')
-
-class CategoryForm(forms.ModelForm):
-    class Meta:
-        model= Category
-        fields= ('name',)
+        fields = ('name', 'body')
--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from blog import views
+
 urlpatterns=[
     path('home/<num>', views.home),
+    path('subscribe', views.subscribe, name ='subscribe'),
+    # path('allPosts', views.PostList.as_view()),
     path('allPosts', views.post_list),
-    path('<category_id>', views.category_posts, name='category_posts'),
-    path('newPost', views.addPost),
-    path('<slug>/', views.post_detail, name='post_detail'),
-    path('<slug>/<commentId>/', views.comment_reply, name="comment_reply"),
-    path('newCategory', views.addCategory)
+    # path('index', views.post_list),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
