2c2cff64c4490635bc6b3674df041cccf646fc06
Ahmed-Abd-elhamid <eng.a.abdelhamid@outlook.com>
2020-02-24 07:01:59 +0200

    Connecting some pages with each other

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from blog import views
 
 urlpatterns=[
-    path('home/<num>', views.home),
-    path('subscribe', views.subscribe, name ='subscribe'),
+    path('home/', views.home, name='home'),
+    path('sub/<category_id>', views.subscribe, name ='subscribe'),
+    path('unsub/<category_id>', views.unsubscribe, name ='unsubscribe'),
+    path('cat/<category_id>', views.category_posts, name='category_posts'),
     # path('allPosts', views.PostList.as_view()),
-    path('allPosts', views.post_list),
+    path('allPosts', views.post_list, name='allPosts'),
     # path('index', views.post_list),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
